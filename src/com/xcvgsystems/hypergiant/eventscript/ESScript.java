9a63ac9e51990efa2067480984e08cd2556edb83
XCVG <xcvgvcx@gmail.com>
2016-07-07 14:37:57 -0700

    added (dummy) ESScript class

---
+++ src/com/xcvgsystems/hypergiant/eventscript/ESScript.java
@@ -0,0 +1,12 @@
+package com.xcvgsystems.hypergiant.eventscript;
+
+/**
+ * An EventScript script object.
+ * 
+ * @author Chris
+ *
+ */
+public class ESScript
+{
+	//TODO: store state, actual script, etc here
+}
