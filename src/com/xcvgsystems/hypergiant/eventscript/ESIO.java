99b3e8689cc43733ab7cb27bbd141c0d143f1b96
XCVG <xcvgvcx@gmail.com>
2016-07-14 10:14:14 -0700

    started working on the ESIO class for no good reason at all

---
+++ src/com/xcvgsystems/hypergiant/eventscript/ESIO.java
@@ -0,0 +1,19 @@
+package com.xcvgsystems.hypergiant.eventscript;
+
+import java.io.*;
+
+/**
+ * EventScript standard IO class, abstracts stdout, stdin, and stderr streams so they can go different places.
+ * Some of this will be engine-dependent.
+ * 
+ * @author Chris
+ *
+ */
+public class ESIO
+{
+	protected static InputStream in;
+	protected static PrintStream out;
+	protected static PrintStream err;
+	
+
+}
